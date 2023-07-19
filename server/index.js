720dba6cdae04e9faa90fa2edcecd5b092bed615
chojeongeun <juy0816@naver.com>
2023-07-19 11:06:33 +0900

    몽고디비 접속 코드 추가

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const app = express();
 const port = 5000;
 
-//express에서 client 측 build 폴더까지의 경로를 static하게 지정
+//express에서 react안쪽 build폴더까지의 경로를 static으로 지정
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.listen(port, () => {
+	mongoose
+		.connect('mongodb+srv://juy0816:!abcd1234@cluster0.e6eepiz.mongodb.net/')
+		//접속 성공시
+		.then(() => console.log(`Server app listening on port ${port} with MongoDB`))
+		//접속 실패시
+		.catch((err) => console.log(err));
+});
+
 app.get('/', (req, res) => {
+	//서버에서 5000포트로 접속하면 static폴더로 지정되어 있는 build안쪽의 index.html을 화면에 내보냄
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+//어떤 URL에서 접속하더라도 화면이 뜨도록 설정
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-//경로가 어떤게 들어와도 index.html로 보여줄거다
-//404페이지 - 리액트는 단일페이지로 이상한 url로 들어오더라도 index.html을 보여주어야 404페이지를 보여주기 때문에 어떤게 들어와도 index.html로 설정하기
-
-app.listen(port, () => {
-	console.log(`Server app lisetening on port ${port}`);
-});
-
-//5000포트접속했을때 index.html로 화면을 출력하겠다
