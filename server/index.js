ac4f03b2087866b2626b70bfb9b666bb03a8a8e6
chojeongeun <juy0816@naver.com>
2023-07-19 11:42:46 +0900

    리액트, 서버간의 데이터 전달

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
+//클라리언트로 부터 보내진 데이터를 전달받도록 설정 (body-parser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //express에서 react안쪽 build폴더까지의 경로를 static으로 지정
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -15,13 +19,17 @@ app.listen(port, () => {
 		//접속 실패시
 		.catch((err) => console.log(err));
 });
-
 app.get('/', (req, res) => {
 	//서버에서 5000포트로 접속하면 static폴더로 지정되어 있는 build안쪽의 index.html을 화면에 내보냄
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 //어떤 URL에서 접속하더라도 화면이 뜨도록 설정
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+//리액트로부터 전달된 요청 라우터
+app.post('/api/send', (req, res) => {
+	console.log(req.body);
+	res.json({ success: true, result: req.body.name + '2' });
 });
