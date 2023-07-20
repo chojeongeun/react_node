d80626396c3989083d8055e3a66a2951e832c8b0
chojeongeun <juy0816@naver.com>
2023-07-20 10:44:53 +0900

    컴포넌트 분리 및 리액트 라우터 설정

---
+++ client/src/common/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+	return <Main>main</Main>;
+}
+
+export default Main;
---
+++ client/src/community/Create.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function Create() {
+	const [Tit, setTit] = useState('');
+	const [Con, setCon] = useState('');
+	const handleCreate = () => {
+		const item = { title: Tit, content: Con };
+
+		axios
+			.post('/api/create', item)
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err));
+	};
+	useEffect(() => {}, []);
+
+	return (
+		<section>
+			<label htmlFor='tit'>Title</label>
+			<input type='text' id='tit' value={Tit} onChange={(e) => setTit(e.target.value)} /> <br />
+			<label htmlFor='con'>Content</label>
+			<textarea
+				name='con'
+				id='con'
+				cols='30'
+				rows='3'
+				value={Con}
+				onChange={(e) => setCon(e.target.value)}
+			></textarea>{' '}
+			<br />
+			<button onClick={handleCreate}>SEND</button>
+		</section>
+	);
+}
+
+export default Create;
+
+/*
+Create (저장)
+Read (읽기)
+Update (수정)
+Delete (삭제)
+*/
