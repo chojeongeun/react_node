0925ef5e6c07117e10ad2d2d016d8f3890044c0f
chojeongeun <juy0816@naver.com>
2023-07-20 11:41:14 +0900

    DB로부터 목록 출력

--- client/src/GlobalStyle.js
+++ client/src/GlobalStyle.js
@@ -2,12 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   * {
-    margin: 0; padding: 0; box-sizing: border-box;
+    margin: 0%; padding: 0; box-sizing: border-box;
   }
-  ul,ol,li{
+  ul,ol,li {
     list-style: none;
   }
-  a{
+  a {
     text-decoration: none; color: #777;
   }
 `;
--- client/src/community/List.js
+++ client/src/community/List.js
@@ -1,6 +1,13 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function List() {
+	useEffect(() => {
+		axios.post('/api/read').then((res) => {
+			console.log(res);
+		});
+	}, []);
 	return <Layout name={'List'}>List</Layout>;
 }
 
