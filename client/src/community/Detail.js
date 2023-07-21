6cc59ee8d1d1136861a6d7810bcabb793582770e
chojeongeun <juy0816@naver.com>
2023-07-21 11:19:54 +0900

    상세 페이지 내용 출력 & community전용 라우터 추가 & 요청 라우터 변경 & 글목록 요청 라우터 메서드 방식 변경

--- client/src/community/Detail.js
+++ client/src/community/Detail.js
@@ -1,10 +1,32 @@
 import Layout from '../common/Layout';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Detail() {
 	const params = useParams();
 	console.log(params);
-	return <Layout name={'Detail'}>Detail</Layout>;
+	const [Detail, setDetail] = useState(null); //객체값 가져올거니까 null로 초기화
+
+	useEffect(() => {
+		axios
+			.post('/api/community/detail', params)
+			.then((res) => {
+				if (res.data.success) {
+					setDetail(res.data.detail);
+				} else {
+					alert('상세글 호출에 실패했습니다.');
+				}
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	return (
+		<Layout name={'Detail'}>
+			<h2>{Detail?.title}</h2>
+			<p>{Detail?.content}</p>
+		</Layout>
+	);
 }
 
 export default Detail;
--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
-const { Post } = require('./model/postSchema.js');
-const { Counter } = require('./model/counterSchema.js');
 
 //클라이언트로 부터 보내진 데이터를 전달받도록 설정 (body-parser)
 app.use(express.json());
@@ -12,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 
 //express에서 react안쪽 build폴더까지의 경로를 static으로 지정
 app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use('/api/community', require('./router/communityRouter.js'));
 
 app.listen(port, () => {
 	//db설정하기 위해 미리 설정해둠
@@ -29,44 +29,4 @@ app.get('/', (req, res) => {
 //어떤 URL에서 접속하더라도 화면이 뜨도록 설정
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-//create
-//글 저장 작업 흐름
-//Counter모델에서부터 글 번호 가져옴 -> body-parser로 제목, 본문 가져와서 글 번호를 추가한 후 모델 인스턴스 저장
-//저장이 완료되면 카운터 모델에 있는 글번호 값을 증가
-//findone은 특정한 다큐먼트 하나를 콕 집는거기때문에 조건이 필요함!
-app.post('/api/create', (req, res) => {
-	Counter.findOne({ name: 'counter' })
-		.exec()
-		.then((doc) => {
-			const PostModel = new Post({
-				title: req.body.title,
-				content: req.body.content,
-				communityNum: doc.communityNum,
-			});
-
-			PostModel.save().then(() => {
-				//update : $inc(증가), $dec(감소), $set(새로운값으로 변경)
-				Counter.updateOne({ name: 'counter' }, { $inc: { communityNum: 1 } })
-					.then(() => {
-						res.json({ success: true });
-					})
-					.catch(() => res.json({ success: false }));
-			});
-		});
-});
-
-//read
-app.post('/api/read', (req, res) => {
-	Post.find() //호출
-		.exec()
-		.then((doc) => {
-			console.log(doc);
-			res.json({ success: true, communityList: doc });
-		})
-		.catch((err) => {
-			console.log(err);
-			res.json({ success: false });
-		});
 });
