54fdb56e502fe797ebc8eefcb344bf4dff7f8e91
chojeongeun <juy0816@naver.com>
2023-07-24 10:57:07 +0900

    글 수정 완료

--- client/src/community/Edit.jsx
+++ client/src/community/Edit.jsx
@@ -2,39 +2,69 @@ import { useState, useEffect } from 'react';
 import Layout from '../common/Layout';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
 /*
-	글수정 흐름
-	1.Edit 컴포넌트 접속하자마자 글 고유번호에 해당하는 데이터를 서버로부터 전달받음
-	2.전달받은 데이터를 각각 제목, 본문, State에 옮겨 담아서 폼요소 안에 출력
-	3.폼 안에 있는 값을 변경 후 update 버튼 클릭시 해당 데이터값을 하나의 객체로 묶어서 서버쪽에 전달
-	4.서버쪽에서는 해당 body-parser객체를 받아서 updateOne메서드로 데이터 수정 (이때 $set방식으로 해야됨)
-	5.수정이 완료되면 다시 프론트쪽에 성공 메세지 전달
+  글수정 흐름
+  1.Edit컴포넌트 접속하자마자 글 고유번호에 해당하는 데이터를 서버로부터 전달받음
+  2.전달받은 데이터를 각각 제목, 본문 State에 옮겨담아서 폼요소안에 출력
+  3.폼안에 있는 값을 변경후 update버튼 클릭시 해당 데이터값을 하나의 객체로 묶어서 서버쪽에 전달
+  4.서버쪽에서는 해당 body-parser객체를 받아서 updateOne메서드로 데이터 수정 ($set방식)
+  5.수정이 완료되면 프론트쪽에 성공 메세지 전달
 */
 
 function Edit() {
 	const params = useParams();
 	const [Title, setTitle] = useState('');
 	const [Content, setContent] = useState('');
+	const [Detail, setDetail] = useState({});
+
+	const handleUpdate = () => {
+		if (Title.trim() === '' || Content.trim() === '') return alert('모든 항목을 입력하세요.');
+
+		const item = {
+			title: Title,
+			content: Content,
+			id: params.id,
+		};
+		axios.post('/api/community/edit', item).then((res) => {
+			if (res.data.success) {
+				alert('글 수정이 완료되었습니다.');
+			} else {
+				alert('글 수정에 실패했습니다.');
+			}
+		});
+	};
 
 	useEffect(() => {
 		axios.post('/api/community/detail', params).then((res) => {
 			if (res.data.success) {
 				console.log(res.data.detail);
+				setDetail(res.data.detail);
 			}
 		});
 	}, []);
 
+	useEffect(() => {
+		//서버쪽으로 새로운 응답이 넘어오자마자
+		console.log(Detail);
+		setTitle(Detail.title);
+		setContent(Detail.content);
+	}, [Detail]);
+
 	return (
 		<Layout name={'Post'}>
 			<label htmlFor='tit'>Title</label>
-			<input type='text' id='tit' value={Title} onChange={(e) => setTitle(e.target.value)} /> <br />
+			<input type='text' id='tit' value={Title || ''} onChange={(e) => setTitle(e.target.value)} /> <br />
 			<label htmlFor='con'>Content</label>
-			<textarea id='con' cols='30' rows='3' value={Content} onChange={(e) => setContent(e.target.value)}></textarea>
+			<textarea
+				id='con'
+				cols='30'
+				rows='3'
+				value={Content || ''}
+				onChange={(e) => setContent(e.target.value)}
+			></textarea>
 			<br />
-			<button>UPDATE</button>
+			<button onClick={handleUpdate}>UPDATE</button>
 		</Layout>
 	);
 }
-
 export default Edit;
--- server/router/communityRouter.js
+++ server/router/communityRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { Post } = require('../model/postSchema.js');
 const { Counter } = require('../model/counterSchema.js');
-
 //글저장 라우터
 router.post('/create', (req, res) => {
 	Counter.findOne({ name: 'counter' })
@@ -13,7 +12,6 @@ router.post('/create', (req, res) => {
 				content: req.body.content,
 				communityNum: doc.communityNum,
 			});
-
 			PostModel.save().then(() => {
 				//update : $inc(증가), $dec(감소), $set(새로운값으로 변경)
 				Counter.updateOne({ name: 'counter' }, { $inc: { communityNum: 1 } })
@@ -24,7 +22,6 @@ router.post('/create', (req, res) => {
 			});
 		});
 });
-
 //목록 출력 라우터
 router.get('/read', (req, res) => {
 	Post.find()
@@ -38,13 +35,27 @@ router.get('/read', (req, res) => {
 			res.json({ success: false });
 		});
 });
-
 //상세페이지 출력 라우터
 router.post('/detail', (req, res) => {
 	Post.findOne({ communityNum: req.body.id })
 		.exec()
 		.then((doc) => res.json({ success: true, detail: doc }))
 		.catch((err) => res.json({ success: false, err: err }));
+});
+
+//글 수정요청 라우터
+router.post('/edit', (req, res) => {
+	const temp = {
+		title: req.body.title,
+		content: req.body.content,
+	};
+	Post.updateOne({ communityNum: req.body.id }, { $set: temp })
+		.exec()
+		.then((doc) => {
+			console.log(doc);
+			res.json({ success: true });
+		})
+		.catch((err) => res.json({ success: false }));
 });
 
 module.exports = router;
